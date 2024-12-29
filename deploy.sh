66377d116c748c95570354b1fc92fadcb86ee57b
thee <theekshana.sandaru@gmail.com>
2024-12-29 23:49:18 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,10 @@ echo "appliyng deployment yml"
 kubectl apply -f deployment.yaml
 
 echo "removing unused docker containers"
-docker container prune
+docker container prune -f
 
 echo "removing unused docker images"
-docker image prune -a
+docker image prune -f
+
+echo "forwarding port"
+nohup kubectl port-forward svc/patient-service-api-service 3000:3000 > port-forward.log 2>&1 &
